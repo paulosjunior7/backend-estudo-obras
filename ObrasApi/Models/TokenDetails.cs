850b696cbbe7dd6d4e3ecdb0cfbbc24a9d02fa06
paulosousa <jovemp@hotmail.com>
2024-06-30 10:49:59 -0300

    Adicionado dados do usuario no login

--- ObrasApi/Models/TokenDetails.cs
+++ ObrasApi/Models/TokenDetails.cs
@@ -5,6 +5,8 @@
     public class TokenDetails
     {
         public string UserId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
         public string Token { get; set; }
         public DateTime ExpireOn { get; set; }
     }
